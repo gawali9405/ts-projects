5ad22289edd6dec35f5305d8c8e7008e3fb84d88
sandip gawali <gawalisandip150@gmail.com>
2025-05-14 12:51:15 +0530

    created handleRemoveChnage funcion

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,20 @@ function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("Sandip");
 
-  const handleChange = () => {
-    if (count === 100) { 
-       alert("The value cannot go beyond 100");
+  const handleAddChange = () => {
+    if (count === 100) {
+      alert("The value cannot go beyond 100");
       return;
     }
     setCount((count) => count + 20);
+  };
+
+  const handleRemoveChange = () => {
+    if (count === 0) {
+      alert("The value cannot go below 0");
+      return;
+    }
+    setCount((count) => count - 20);
   };
 
   return (
@@ -18,10 +26,8 @@ function App() {
       <h1>React Counter</h1>
       <div className="card">
         <h1>{count}</h1>
-        <button onClick={handleChange}>Add Counter</button>
-        <button onClick={() => setCount((count) => count - 20)}>
-          Remove Counter
-        </button>
+        <button onClick={handleAddChange}>Add Counter</button>
+        <button onClick={handleRemoveChange}>Remove Counter</button>
       </div>
       <h1>{name}</h1>
       <input
